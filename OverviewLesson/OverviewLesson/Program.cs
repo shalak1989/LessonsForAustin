92ba2d319142c3a0efe38fa66faaa8157dff95be
James Ware <jazz642@gmail.com>
2018-06-20 20:01:22 -0400

    initial commit

---
+++ OverviewLesson/OverviewLesson/Program.cs
@@ -0,0 +1,43 @@
+﻿using CalculateStuff;
+using System;
+using System.Collections.Generic;
+
+namespace OverviewLesson
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var program = new Program();
+      var nameList = new List<string>() { "austin", "james" };
+      var randomNumber = 0;
+      program.ModifyList(nameList, randomNumber);
+      //foreach (var item in nameList)
+      //{
+      //  Console.WriteLine(item);
+      //}
+
+      //Console.WriteLine(randomNumber);
+
+
+
+
+      var number1 = 1000000000;
+      var number2 = 1000000000;
+
+      var calculator = new Calculator();
+      var answer = calculator.Multiply(number1, number2);
+      Console.WriteLine(answer);
+
+      Console.ReadLine();
+    }
+
+    public void ModifyList(List<string> listOfNames, int number)
+    {
+      number = 100;
+      listOfNames.Add("bob");
+      listOfNames = new List<string>() { "Taco" };
+      listOfNames.Add("ali");
+    }
+  }
+}
