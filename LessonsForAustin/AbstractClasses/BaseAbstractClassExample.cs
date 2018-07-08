5bfcb82f59ad3c5d7f16c30475381241541610a8
James Ware <jazz642@gmail.com>
2018-07-08 11:46:36 -0400

    added in more helpful examples

--- LessonsForAustin/AbstractClasses/BaseAbstractClassExample.cs
+++ LessonsForAustin/AbstractClasses/BaseAbstractClassExample.cs
@@ -19,5 +19,33 @@ namespace AbstractClasses
     {
       return x * y;
     }
+
+    public virtual int Multiply(List<int> numbers)
+    {
+      int answer = 1;
+      foreach (var number in numbers)
+      {
+        answer = answer * number;
+      }
+      return answer;
+    }
+
   }
 }
+
+
+//Some random LINQ stuff below:
+
+//var findNumber5 = numbers.Where(x => x == 5);
+
+////this foreach loop is the same thing as the lambda expression above
+//var fiveCount = 0;
+//      foreach (var x in numbers)
+//      {
+//        if (x == 5)
+//        {
+//          fiveCount++;
+//        }
+//      }
+
+//      Console.WriteLine("I found " + findNumber5.Count() + " 5's");
--- LessonsForAustin/AbstractClasses/Program.cs
+++ LessonsForAustin/AbstractClasses/Program.cs
@@ -11,7 +11,8 @@ namespace AbstractClasses
     static void Main(string[] args)
     {
       var example = new DerivedClassExample();
-      Console.WriteLine(example.Multiply(20, 20));
+      Console.WriteLine(example.Multiply(new List<int> { 1, 2, 3, 4, 6, 5, 5, 5, 5}));
+      Console.ReadLine();
     }
   }
 }
