eca34145a40fb3f3afde86f49118a0994004e170
James Ware <jazz642@gmail.com>
2018-07-08 12:27:45 -0400

    added in more colletions

--- LessonsForAustin/AbstractClasses/DerivedClassExample.cs
+++ LessonsForAustin/AbstractClasses/DerivedClassExample.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AbstractClasses
 {
-  class DerivedClassExample : BaseAbstractClassExample
+  public class DerivedClassExample : BaseAbstractClassExample
   {
     public override int Subtract(int x, int y)
     {
